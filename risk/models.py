a7d86e5ab0c9ea5b17cd8434ce17d57f4742485b
liuxkaren <lukaren2266@gmail.com>
2024-05-23 18:50:48 +0800

    create earthquake data.

--- risk/models.py
+++ risk/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Earthquake(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    latitude = models.DecimalField(max_digits=8, decimal_places=5)
+    longitude = models.DecimalField(max_digits=8, decimal_places=5)
+    depth = models.DecimalField(max_digits=5, decimal_places=2)
+    scale = models.DecimalField(max_digits=5, decimal_places=2)
+    city = models.TextField(max_length=100)
+    level = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.date} {self.time} - {self.city} - {self.level}"
