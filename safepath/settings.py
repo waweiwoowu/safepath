2e7ce0a86bb1b1ccab02c83b70d7375f2a2b5969
fredshih7 <f37854979@gmail.com>
2024-05-28 21:14:24 +0800

    removed weird stuff
    idk why some codes appeared after i had merged from main, like
    <<<<<<< HEAD
    =======
    >>>>>>> main

--- safepath/settings.py
+++ safepath/settings.py
@@ -16,27 +16,21 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-<<<<<<< HEAD
 STATIC_URL = '/static/'
 
-=======
 # Test: static path
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 
 # Uncomment the next lines if you have additional static files directories
->>>>>>> main
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-<<<<<<< HEAD
 STATIC_ROOT = BASE_DIR / "staticfiles"
-=======
 # Static files will be collected here for production
 STATIC_ROOT = BASE_DIR / "staticfiles"
 # Test: static path
->>>>>>> main
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
