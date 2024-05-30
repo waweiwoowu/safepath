0077569bf732fcfd8fbd4c68e72cf689b1873225
fred4117 <fled4117@gmail.com>
2024-05-30 14:30:19 +0800

    Caraccident-complete 2/3

--- explorer/tests.py
+++ explorer/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .maps import Direction
+from maps import Direction
 from asgiref.sync import sync_to_async
 import asyncio
 
@@ -8,7 +8,7 @@ async def main():
     # origin = "Place A"
     # destination = "Place B"
     # direction = Direction(origin, destination)
-
+    
     # Direction() without arguments is a template Direction object where origin = "台北101" and destination = "台北市立動物園"
     direction = Direction()
     print(direction.data)
